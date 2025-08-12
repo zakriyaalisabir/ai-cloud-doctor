abbaf2f3b30c5064b2fcdda0df0e7245dc054dd7
Zakriya Ali Sabir (ALI) <zakriya.alisabir@accor.com>
2025-08-13 00:35:39 +0700

    test(jest): add jest configuration for TypeScript ESM support and coverage

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  },
+  moduleNameMapping: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/cli.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  testMatch: [
+    '**/__tests__/**/*.test.ts'
+  ]
+};
